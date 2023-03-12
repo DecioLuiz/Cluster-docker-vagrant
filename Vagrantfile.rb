5ea9f0db091e483d0c7d5862d365494a78f84726
DecioLuiz <decioluiz@msn.com>
2023-03-12 14:42:33 -0300

    Criação do Vagrantfile e definição das máquinas virtuais

---
+++ Vagrantfile.rb
@@ -0,0 +1,7 @@
+machines = {
+  "master" => {"memory" => "1024", "cpu" => "1", "ip" => "100", "image" => "ubuntu/focal64"},
+  "node01" => {"memory" => "1024", "cpu" => "1", "ip" => "101", "image" => "ubuntu/focal64"},
+  "node02" => {"memory" => "1024", "cpu" => "1", "ip" => "102", "image" => "ubuntu/focal64"},
+  "node04" => {"memory" => "1024", "cpu" => "1", "ip" => "103", "image" => "ubuntu/focal64"}
+}
+
